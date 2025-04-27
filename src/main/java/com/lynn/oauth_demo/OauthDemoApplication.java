2bc04502acf108990da89159cfd1747d3cd80772
Lynn <lynn990908@gmail.com>
2025-04-27 12:57:15 +0800

    fix properties

--- src/main/java/com/lynn/oauth_demo/OauthDemoApplication.java
+++ src/main/java/com/lynn/oauth_demo/OauthDemoApplication.java
@@ -3,9 +3,11 @@ package com.lynn.oauth_demo;
 import com.github.lianjiatech.retrofit.spring.boot.core.RetrofitScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @RetrofitScan(basePackages = "com.lynn.oauth_demo.client")
+@PropertySource("classpath:application-secret.properties")
 public class OauthDemoApplication {
 
   public static void main(String[] args) {
